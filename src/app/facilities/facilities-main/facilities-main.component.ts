97c9c4331aedd7b9c67b23ecc081d3254be275f7
sarwin-github <dapito.sherwin@yahoo.com>
2018-07-10 14:29:41 +0800

    added facilities

--- src/app/facilities/facilities-main/facilities-main.component.ts
+++ src/app/facilities/facilities-main/facilities-main.component.ts
@@ -2,16 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { fadeIn } from '../../animations/fadeIn';
 
 @Component({
-  selector: 'app-facilities-main',
-  animations: [fadeIn],
-  templateUrl: './facilities-main.component.html',
-  styleUrls: ['./facilities-main.component.scss']
+	selector: 'app-facilities-main',
+	animations: [fadeIn],
+	templateUrl: './facilities-main.component.html',
+	styleUrls: ['./facilities-main.component.scss']
 })
 export class FacilitiesMainComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		let div = document.getElementById('facilities').offsetTop;
+		window.scrollTo({ left: 0, top: div - 100, behavior: 'smooth' });
+	}
 
 }
